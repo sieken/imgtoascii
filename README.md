b692c173397d7784f9ec8b91a32280afbc8412ed
David Henriksson <davhen@kth.se>
2020-08-07 01:37:46 +0200

    First ASCII output
    Seems to work somewhat fine with greyscale for now. Should add cli args
    parsing asap though.

--- README.md
+++ README.md
@@ -3,17 +3,11 @@ Turn images into ascii art ~representations~
 
 ## TODO
 
-Read and parse
-* [x] Read image from path
-    * Used `jimp`
-* [x] Return some kind of list of avg grayscale
-    * `jimp`s bitmap
-
-Convert to blocks
-* [ ] Read grayscaled list
-* [ ] Return blocks of averages (e.g. 9x9 pixel blocks averaged out to one color)
-
-Output
-* [ ] Take list of grayscaled blocks
-* [ ] Map block color/grayscale value to matching density ascii character
-* [ ] Replace blocks with mapped ascii characters
+* [x] Read image
+* [x] Grayscale
+* [x] Map to ascii value
+* [x] Output ascii art
+* [ ] CLI argument parsing
+* [ ] Color
+* [ ] Resize
+* [ ] Pixel-by-pixel or blockify?
--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-
-
-// TODO
-// 	[x] Read image from path
-// 	[x] Return some kind of list of avg greyscale
-//
-// 	[ ] Read grayscaled lisst
-// 	[ ] Return blocks of averages
-//
-// 	[ ] Take list of grayscaled blocks
-// 	[ ] Map block color value to matching density ascii
-// 	[ ] Replace blocks with mapped ascii character and output
-//
-
 const jimp = require('jimp')
+const testfileInPath = 'test/test.png'
 
-jimp.read('test/test.png')
+jimp.read(testfileInPath)
 	.then(img => {
-		// This is list of grayscale pixels to blockify
-		return blockify(img.bitmap.data)
+		const scale = 0.5
+		const stretch = 1.8
+		return img
+			.resize(img.getWidth() * scale * stretch, img.getHeight() * scale)
+			.greyscale()
+	})
+	.then(img => {
+		let width = img.getWidth()
+		let height = img.getHeight()
+		let asciiString = ''
+		for (let y = 0; y < height; y++) {
+			for (let x = 0; x < width; x++) {
+				const greyscaleVal = jimp.intToRGBA(img.getPixelColor(x, y)).r
+				asciiString += matchAscii(greyscaleVal, standardRampChars)
+			}
+			asciiString += '\n'
+		}
+		console.log(asciiString)
 	})
 	.catch(err => { console.error(err) })
 
-blockify = pixelList => {
-	console.log('Got me a juicy list of pixels\n', pixelList)
+// Helpers
+
+matchAscii = (pixelVal, asciiSet) => {
+	const rangeMax = asciiSet.length - 1 // largest indexable entry
+	// since both input (0-255) and output (0-rangeMax)
+	// start from 0, we can get a normalized value by this
+	const mapped = Math.round((rangeMax/255) * pixelVal)
+	return asciiSet[mapped]
 }
+
+// "Standard ramp chars" from:
+// http://paulbourke.net/dataformats/asciiart/
+const standardRampChars = [
+  ' ', '.', "'", '`', '^', '"', ',', ':', ';',
+  'I', 'l', '!', 'i', '>', '<', '~', '+', '_',
+  '-', '?', ']', '[', '}', '{', '1', ')', '(',
+  '|', '/', 't', 'f', 'j', 'r', 'x', 'n', 'u',
+  'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L',
+  'Q', '0', 'O', 'Z', 'm', 'w', 'q', 'p', 'd',
+  'b', 'k', 'h', 'a', 'o', '*', '#', 'M', 'W',
+  '&', '8', '%', 'B', '@', '$'
+]
